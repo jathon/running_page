8802ada41c4eee2eee1ce12713da9a6da29827de
jathon <easife@gmail.com>
2021-02-18 19:30:12 +0800

    update mapbox token

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1Ijoiam9lemh1IiwiYSI6ImNrazl4N213bzBnOGYydnBuamd3bnNuZG8ifQ.3aaH0QYzZ-mp8ZdJmJqu7A';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
